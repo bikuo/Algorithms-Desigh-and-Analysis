e9cc2093741312f1006eddf9f66a373a6788b388
williamkuo <lock3262000@gmail.com>
2014-10-13 00:07:28 +0800

    comments

--- 2/2.cpp
+++ 2/2.cpp
@@ -16,81 +16,78 @@ bool myfunc(Box a,Box b){
 }
 std::vector<Box>::iterator it;
 vector<Box> temp(100000);
-void mergee(vector<Box>& vv,const int start,const int mid,const int end){
+void mergee(vector<Box>& vec,const int start,const int mid,const int end){
 	int i = start;
 	int j = mid + 1;
 	int curr = 0;
 	int pin;
     int tag;
     int index = start;
-        while(i<= mid && j<=end){
-            if(vv[i].wid < vv[j].wid){
-				counter += end - j + 1;
-                temp[curr]=vv[i];
+    while(i<= mid && j<=end){
+        if(vec[i].wid < vec[j].wid){
+            counter += end - j + 1;
+            temp[curr]=vec[i];
+            curr++;
+            i++;
+        }
+        else if(vec[i].wid == vec[j].wid){
+            pin=j;// clone the value of j
+            tag=i;// clone the value of i
+            if(vec[i].len < vec[j].len){
+                counter += end-j+1;
+                temp[curr] = vec[i];
                 curr++;
                 i++;
-			}
-			else if(vv[i].wid == vv[j].wid){
-                pin=j;
-                tag=i;
-                if(vv[i].len < vv[j].len){
-                    counter += end-j+1;
-                    temp[curr] = vv[i];
+            }
+            else if(vec[i].len == vec[j].len){
+                while (pin<= end && vec[i].len == vec[pin].len && vec[i].wid == vec[pin].wid)//find boxes with same sizes on the right
+                    pin++;
+                while (tag<= mid && vec[tag].len == vec[j].len && vec[tag].wid == vec[j].wid)//search for boxes with same sizes on the left
+                    tag++;
+                counter += (end-pin+1)*(tag-i);
+                while(i<tag){
+                    temp[curr] = vec[i];
                     curr++;
                     i++;
                 }
-                else if(vv[i].len == vv[j].len){
-                    while (pin<= end && vv[i].len == vv[pin].len && vv[i].wid == vv[pin].wid){
-                        pin++;
-                    }
-                    while (tag<= mid && vv[tag].len == vv[j].len && vv[tag].wid == vv[j].wid){
-                        tag++;
-                    }
-                    counter += (end-pin+1)*(tag-i);
-                    while(i<tag){
-                        temp[curr] = vv[i];
-                        curr++;
-                        i++;
-                    }
-                    
-                }
             }
-            else if(vv[i].wid > vv[j].wid){
-			temp[curr] = vv[j];
+        }
+        else if(vec[i].wid > vec[j].wid){
+			temp[curr] = vec[j];
 			curr++;
 			j++;
 			counter += (mid - i + 1)*2;
 		}
 	}
 	while(i<=mid){
-		temp[curr] = vv[i];
+		temp[curr] = vec[i];
 		curr++;
 		i++;
 	}
 	while(j<=end){
-		temp[curr] = vv[j];
+		temp[curr] = vec[j];
 		curr++;
 		j++;
 	}
-	for(int k=0;k<curr;k++){
-		vv[index] = temp[k];
+	for(int k=0;k<curr;k++){ //put elements sorted back to original vector
+		vec[index] = temp[k];
 		index++;
 	}
 }
-void mergesort(vector<Box>& vv,const int start,const int end){
+void mergesort(vector<Box>& vec,const int start,const int end){
 	if(start == end)
 		return;
 	int mid  = start + (end-start)/2;
-	mergesort(vv,start,mid);
-	mergesort(vv,mid+1,end);
-	mergee(vv,start,mid,end);
+	mergesort(vec,start,mid);
+	mergesort(vec,mid+1,end);
+	mergee(vec,start,mid,end);
 }
 int main()
 {
 	int test;
 	long long wide, leng,num;
 	long long int ans;
-	std::vector<Box> vb;
+	std::vector<Box> boxes;
 	
 	scanf("%d", &test);
 	for (int i = 0; i < test; ++i)
@@ -101,13 +98,13 @@ int main()
 			scanf("%lld %lld", &leng , &wide);
 			box.len = (leng > wide )? leng:wide;
 			box.wid = (wide < leng )? wide:leng;
-			vb.push_back(box);
+			boxes.push_back(box);
 		}
-		std::sort(vb.begin(),vb.end(),myfunc);
-        mergesort(vb,0,num-1);
+		std::sort(boxes.begin(),boxes.end(),myfunc);
+        mergesort(boxes,0,num-1);
 		ans = num*(num-1)-counter;
 		printf("%lld\n",ans);
-		vb.clear();
+		boxes.clear();
 		counter = 0;
 	}
 	return 0;
