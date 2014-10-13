f226ef712ec82427b883bbdafae03c318a4f6b2a
williamkuo <lock3262000@gmail.com>
2014-10-13 13:44:56 +0800

    fix

--- 3/3.cpp
+++ 3/3.cpp
@@ -27,8 +27,7 @@ bool myfunc(Nodes a,Nodes b){
 }
 
 Event events[100001];
-int crosscheck[100001];
-int worker[100001],res[100001];
+int worker[100001],res[100001],crosscheck[100001];
 int n,m,q;
 
 void decision(vector<Player> &v, int left,int right){
@@ -36,23 +35,19 @@ void decision(vector<Player> &v, int left,int right){
 		return;
 	if (left == right && left != q){
 		for (int i = 0; i < v.size(); ++i)
-		{
 			res[v[i].num] = left;
-		}
 		return;
 	}
 	int mid = (left+right)/2;
 	Nodes tnode;
-	std::vector<Nodes> linked;
-	for(int j=left;j<=mid;j++){
+	vector<Nodes> linked;
+	for(int j=left;j<=mid;j++){// 
 		tnode.loc = events[j].left-0.5;
 		tnode.val = events[j].goldvol;
 		linked.push_back(tnode);
-
 		tnode.loc = events[j].right+0.5;
 		tnode.val = -events[j].goldvol;
 		linked.push_back(tnode);
-
 	}
 	tnode.val=0;
 	for(int i=0;i<v.size();i++)
@@ -62,9 +57,7 @@ void decision(vector<Player> &v, int left,int right){
 			linked.push_back(tnode);
 			crosscheck[v[i].mine_field[j]]=i;// build a crosscheck table to relate the location to indexes of players
 		}
-
 	sort(linked.begin(), linked.end(),myfunc);
-
 	int summation = 0;
 	std::vector<Player> pre = v;
 	for(int k=0; k<linked.size();k++){
@@ -73,9 +66,8 @@ void decision(vector<Player> &v, int left,int right){
 		else // is a indicator of range of dropping golds
 			summation += linked[k].val;
 	}
-	if(left == right && left == q){
-		for (int i = 0; i < v.size(); ++i)
-		{
+	if(left == right && left == q){// at the last event 
+		for (int i = 0; i < v.size(); ++i){
 			if (v[i].curr < v[i].goal){
 				res[v[i].num] = -1;
 			}
@@ -86,26 +78,20 @@ void decision(vector<Player> &v, int left,int right){
 	}
 	std::vector<Player> win ,lose;
 	for(int i=0;i<v.size();i++){
-		if(v[i].curr <v[i].goal)
+		if(v[i].curr < v[i].goal)
 			lose.push_back(v[i]);
 		else{
 			v[i].curr = pre[i].curr;
 			win.push_back(v[i]);
 		}
 	}
-
 	decision(win,left,mid);
 	decision(lose,mid+1,right);
 }
-
-int main()
-{
+int main(){
 	int test;
-	int count=0;
-	
 	scanf("%d", &test);
-	for (int i = 0; i < test; ++i)
-	{
+	for (int i = 0; i < test; ++i){
 		std::vector<Player> play;
 		scanf("%d %d %d", &n, &m, &q);
 		for (int j = 1; j <= n; ++j){
@@ -132,40 +118,13 @@ int main()
 			events[j] = a;
 		}
 		decision(play,1,q);
-		/*for(vector<Event>::iterator it=events.begin();it!=events.end();it++){
-			count++;
-			for(int i=it->left;i<=it->right;i++){
-				play[(blocks[i-1].belongs_to)-1].curr += it->goldvol;
-			}
-			for(std::vector<Player>::iterator point=play.begin();point != play.end();point ++){
-				if(point->curr >= point->goal && point->status < 0){
-					point->status =1;
-					point->succ=count;
-				}
-
-			}
-
-		}
-		for(std::vector<Player>::iterator it=play.begin();it!=play.end();it++){
-			if(it ==play.begin())
-				printf("%lld", it->succ);
-			else
-				printf(" %lld",it->succ);
-
-		}
-		printf("\n");
-		blocks.clear();
-		events.clear();
-		play.clear();
-		count = 0;*/
-		for(int i=1;i<=n;i++){
+		for(int i=1;i<=n;i++){//print out result 
 			if(i==1)
 				printf("%d", res[i]);
 			else
 				printf(" %d", res[i]);
 		}
 		printf("\n");
-		play.clear();
 	}
 	return 0;
 }
