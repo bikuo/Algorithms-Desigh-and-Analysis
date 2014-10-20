b822b62705d404ae98dea29e3420f2588a6ec817
williamkuo <lock3262000@gmail.com>
2014-10-21 01:31:33 +0800

    editted comments

--- 4/4.cpp
+++ 4/4.cpp
@@ -2,9 +2,8 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-
 using namespace std;
-
+// function to reverse the order of a given string reference from the NET
 void reverse(char str[]){
     int i,j;
     char temp[2020];
@@ -15,7 +14,6 @@ void reverse(char str[]){
     temp[j]='\0';
     strcpy(str,temp);
 }
-
 void lcs(char* a, char* b){
 	int m = strlen(a);
 	int n = strlen(b);
@@ -24,7 +22,9 @@ void lcs(char* a, char* b){
 	char I[m+1][n+1];
 	for (int i=0; i<=m; i++)
     	for (int j=0; j<=n; j++){
-       		//initializing steps on border of maps
+       		//initializing steps on border(when indices i or j equals zero) 
+       		//of 3maps M(max length), D(direction for backtracing), 
+       		//I(initial character of current lcs)
        		if (i == 0 || j == 0){
          		M[i][j] = 0;
          		D[i][j] = '.';
@@ -32,12 +32,15 @@ void lcs(char* a, char* b){
          	}
   			//doing real work 
        		else{
-       			// two stings matches
+       			// two stings matches at a<i-1> b<j-1>
        			if (a[i-1] == b[j-1]){
          			M[i][j] = M[i-1][j-1] + 1;
          			D[i][j] = '\\';
          			I[i][j] = a[i-1];
        			}
+       			//does not match, we compare first the maximum length (choose the longer one)
+       			// then the lexicograghical order (the preceeding one) of entries adjacent
+       			// to the current entry either (i-1) or (j-1)
        			else if(a[i-1] != b[j-1]){
          			if(M[i-1][j] > M[i][j-1]){
          				M[i][j] = M[i-1][j];
@@ -52,12 +55,12 @@ void lcs(char* a, char* b){
          			else if(M[i-1][j] == M[i][j-1]){
          				if(I[i-1][j] > I[i][j-1]){
          					M[i][j] = M[i][j-1];
-         					D[i][j] = '<';
+         					D[i][j] = '<'
          					I[i][j] = I[i][j-1];
          				}
          				else{
          					M[i][j] = M[i-1][j];
-         					D[i][j] = '^';
+         					D[i][j] = '^'; 
          					I[i][j] = I[i-1][j];
          				}
          			}
@@ -65,61 +68,21 @@ void lcs(char* a, char* b){
          	
          	}
      	}
-	// printf out the map constructed above to check for correctness 
-	/*for(int i= 0;i<=m;i++){
-		for(int j=0;j <= n;j++){
-			printf("%d ", M[i][j]);
-		}
-	cout<<"\n";
-	}
-	cout<<"\n";
-	for(int i= 0;i<=m;i++){
-		for(int j=0;j <= n;j++){
-			printf("%c ", D[i][j]);
-		}
-	cout<<"\n";
-	}
-	cout<<"\n";
-	for(int i= 0;i<=m;i++){
-		for(int j=0;j <= n;j++){
-			printf("%c ", I[i][j]);
-		}
-	cout<<"\n";
-	}
-	cout<<"\n";*/
-	//==================================
-	int index = M[m][n];
+	int index = M[m][n];//length of lcs
 	char lcs1[index+1];
 	lcs1[index] = '\0';
 	int i=m,j=n;
-	//tracing back to get lcs
+	//tracing back and get lcs
 	while(i>0 && j>0){
-		//cout<<i<<";"<<j<<endl;
-		if(D[i][j] == '\\'){
+		if(D[i][j] == '\\')
+		{//if a<i-1> and b<j-1> is a character of lcs, add that character to result string
 			lcs1[index-1] = I[i][j];
 			i--;
 			j--;
 			index--;
-		}/*
-		else if(M[i-1][j] > M[i][j-1])
-			i--;
-		else if(M[i-1][j] < M[i][j-1])
-			j--;
-		else if(M[i-1][j] == M[i][j-1]){
-			if(I[i-1][j] == I[i][j])
-				i--;
-			else if(I[i][j-1] ==  I[i][j])
-				j--;
-			else if(I[i][j] == I[i][j-1] && I[i][j] == I[i-1][j])
-				i--;
-			else if(I[i][j] != I[i][j-1] && I[i][j] != I[i-1][j]){
-				if(I[i-1][j] > I[i][j-1])
-					j--;
-				else
-					i--;
-			}
-		}*/
-		else{
+		}
+		else
+		{ //if not, check if the initial character of the current
 			if(I[i][j] == a[i-1])
 				j--;
 			else if(I[i][j] == b[j-1])
@@ -129,11 +92,9 @@ void lcs(char* a, char* b){
 		}
 			
 	}
-	//cout<<lcs1<<endl;
 	reverse(lcs1);
 	printf("%s\n", lcs1);
-	return;
-		
+	return;		
 }	
 int main(int argc, char const *argv[])
 {
@@ -145,11 +106,8 @@ int main(int argc, char const *argv[])
 		scanf("%s", str1);
 		scanf("%s", str2);
 		reverse(str1);
-		//cout<<str1<<endl;
 		reverse(str2);
-		//cout<<str2<<endl;
 		lcs(str1,str2);
-		//printf("\n");
 	}
 	return 0;
 }
