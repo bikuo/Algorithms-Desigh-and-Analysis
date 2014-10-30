9369064bf2be1f2aa02b5886b2bd51d7b323549a
williamkuo <lock3262000@gmail.com>
2014-10-30 19:35:14 +0800

    whoosh

--- 5/5.cpp
+++ 5/5.cpp
@@ -2,13 +2,14 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-char map[15][15];
+char map[16][16];
 int modder=1000000007;
 int pow_2[17] = { 1,2,4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536 };
-void tetris(char map[15][15],int row,int col){
+void tetris(int row,int col){
 	int count = pow_2[col];
 	// initialize matrix with all zero 
 	static int mat[16][16][32768];
+	//int mat[row+1][col+1][count];
 	memset(mat,0,sizeof(mat));
 	mat[1][0][count-1] = 1;
 	// two variables for later use
@@ -20,7 +21,8 @@ void tetris(char map[15][15],int row,int col){
 			for(int k=0;k<count;k++){
 					if(i > 1 && j==0)
 						mat[i][j][k] = mat[i-1][col][k];
-					if(((k>>(col-1)) & 1) ==1){// 1*1 block or a natural obstacle
+					// 1*1 block or a natural obstacle
+					if(((k>>(col-1)) & 1) ==1){
 						des = 2*(k-b)+1;
               			mat[i][j+1][des] += mat[i][j][k];
               			if(mat[i][j+1][des] >= modder)
@@ -31,7 +33,8 @@ void tetris(char map[15][15],int row,int col){
 			  			if(((k>>(col-1)) & 1) ==1){ // leave a blank
           					des = (k-b)*2;
             				mat[i][j+1][des] += mat[i][j][k];
-            				mat[i][j+1][des] %= modder;
+            				if(mat[i][j+1][des] >= modder)
+            					mat[i][j+1][des] -= modder;
 						}
 						if(((k>>(col-1)) % 2)==0){ // 2*1 vertical 
 				  			des = 2*k+1;
@@ -41,17 +44,17 @@ void tetris(char map[15][15],int row,int col){
 						}
 						// cases with wider blocks 
 						if(j < col-1 && map[i][j+1] !='X'){
-						 	if(((k>>(col-2)) & 3) == 3){ // 1*2 horizontal 
-				  			des = 4*(k-a)+3;
-				  			mat[i][j+2][des] += mat[i][j][k];
-				  			if(mat[i][j+2][des] >= modder)
-				  				mat[i][j+2][des] -= modder;
+						 	if((k>>(col-2)) == 3){ // 1*2 horizontal 
+				  				des = 4*(k-a)+3;
+				  				mat[i][j+2][des] += mat[i][j][k];
+				  				if(mat[i][j+2][des] >= modder)
+				  					mat[i][j+2][des] -= modder;
 							}
 							if(((k>>(col-2)) == 0)){ // 2*2 square
-							des = 4*k+3;
-							mat[i][j+2][des] += mat[i][j][k];
-							if(mat[i][j+2][des] >= modder)
-								mat[i][j+2][des] -= modder;
+								des = 4*k+3;
+								mat[i][j+2][des] += mat[i][j][k];
+								if(mat[i][j+2][des] >= modder)
+									mat[i][j+2][des] -= modder;
 							}
 						}
 					}
@@ -71,7 +74,7 @@ int main(){
 			for(int c = 0;c < col; c++)
 					map[r][c] = str[c];
 		}
-		tetris(map,row,col);
+		tetris(row,col);
 		printf("\n");
 	}
 	return 0;
