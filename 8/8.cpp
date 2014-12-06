04c497a14db854985662128ecc65c5db234e3be1
williamkuo <lock3262000@gmail.com>
2014-12-07 00:32:51 +0800

    aww

--- 8/8.cpp
+++ 8/8.cpp
@@ -11,9 +11,8 @@ typedef struct cell{
 typedef struct coor{
 	int x,y,z;
 }COOR;
-void check(int i,int j,int k,int distance,int bottle);
-static queue<COOR> que;
-static CELL grid[100+2][100+2][100+2];
+void check(int ,int ,int ,int ,int, CELL [][102][102] );
+queue<COOR> que;
 int main(int argc, char const *argv[]){
 	int t,size;
 	scanf("%d", &t);
@@ -22,6 +21,7 @@ int main(int argc, char const *argv[]){
 	while(t--){
 		scanf("%d", &size);
 		COOR start,end;
+		CELL grid[size+2][100+2][100+2];
 		for(int i = 1; i <= size; ++i)
 			for(int j = 1; j<= size; ++j)
 				for(int k = 1; k<= size; k++){
@@ -46,35 +46,33 @@ int main(int argc, char const *argv[]){
 			corrz = top.z;
 			curr_dist = grid[corrx][corry][corrz].dist;
 			curr_drink = grid[corrx][corry][corrz].drink;
-			check(corrx+1,corry,corrz,curr_dist, curr_drink);
-			check(corrx-1,corry,corrz,curr_dist, curr_drink);	
-			check(corrx,corry+1,corrz,curr_dist, curr_drink);
-			check(corrx,corry-1,corrz,curr_dist, curr_drink);	
-			check(corrx,corry,corrz+1,curr_dist, curr_drink);
-			check(corrx,corry,corrz-1,curr_dist, curr_drink);		
+			check(corrx+1,corry,corrz,curr_dist, curr_drink, grid);
+			check(corrx-1,corry,corrz,curr_dist, curr_drink, grid);	
+			check(corrx,corry+1,corrz,curr_dist, curr_drink, grid);
+			check(corrx,corry-1,corrz,curr_dist, curr_drink, grid);	
+			check(corrx,corry,corrz+1,curr_dist, curr_drink, grid);
+			check(corrx,corry,corrz-1,curr_dist, curr_drink, grid);		
 			que.pop();
 		}
 		(grid[end.x][end.y][end.z].dist == -1)? printf("Fail OAQ\n") : printf("%d %d\n", grid[end.x][end.y][end.z].dist, grid[end.x][end.y][end.z].drink);
 	}
 	return 0;
 }
-void check(int i,int j,int k,int distance,int bottle){
+void check(int i,int j,int k,int dis,int bottle, CELL grid[][102][102] ){
 	if(grid[i][j][k].dist == -1 && grid[i][j][k].type != '#'){
 		COOR tmp;
 		tmp.x = i;
 		tmp.y = j;
 		tmp.z = k;
 		que.push(tmp);
-		grid[i][j][k].dist = distance+1;
+		grid[i][j][k].dist = dis+1;
 		grid[i][j][k].drink = (grid[i][j][k].type == 'B')? bottle+1 : bottle;
 	}
-	else if(grid[i][j][k].dist == distance + 1){
-		if(grid[i][j][k].type == 'B'){
+	else if(grid[i][j][k].dist == dis + 1){
+		if(grid[i][j][k].type == 'B')
 			grid[i][j][k].drink = (bottle+1 > grid[i][j][k].drink)? bottle+1 : grid[i][j][k].drink;
-		}
-		else{
+		else
 			grid[i][j][k].drink = (bottle > grid[i][j][k].drink)? bottle : grid[i][j][k].drink;
-		}
 	}
 	return;
 }
