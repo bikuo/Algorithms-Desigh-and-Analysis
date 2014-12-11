85033a45d172e48fb61264421060ed089f12967a
williamkuo <lock3262000@gmail.com>
2014-12-11 23:34:14 +0800

    oo

--- 9/9.cpp
+++ 9/9.cpp
@@ -5,23 +5,20 @@
 #include <iostream> 
 using std::stack;
 using std::vector;
-typedef struct path 
-{
+typedef struct path {
 	int end;
 	int weight;	
 }PATH;
 int node,edge;
-const int twos[31]={1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,
-					131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432
-					,67108864,134217728,268435456,536870912,1073741824};
-std::vector< vector<PATH> > v(100010);
+const int twos[31]={1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824};
+std::vector< vector<PATH> > v;
 stack<int> steak;
 int search(int bits,int sum){
 	if(bits == 0)
 		return 0;
 	int vertex = 1;
 	int pathcount[node+1];
-	int pushed[node+1];
+	bool pushed[node+1];
 	memset(pathcount,0,sizeof(pathcount));
 	memset(pushed,0,sizeof(pushed));
 	steak.push(1);
@@ -33,7 +30,7 @@ int search(int bits,int sum){
 				if( (!pushed[v[cur][pathcount[cur]].end]) && (cur !=v[cur][pathcount[cur]].end) ){
 					steak.push(v[cur][pathcount[cur]].end);
 					vertex++;
-					pushed[v[cur][pathcount[cur]].end] = 1;
+					pushed[v[cur][pathcount[cur]].end] = true;
 				}	
 			}
 			pathcount[cur]++;
@@ -46,14 +43,14 @@ int search(int bits,int sum){
 	else	// okay
 		return search(bits-1, sum);
 }
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	PATH tmp;
 	int t,end1,end2,wt;
 	int max,count;
 	scanf("%d",&t);
 	while(t--){
 		scanf("%d%d", &node, &edge);
+		v.resize(node+1);
 		count = 0;
 		max = 0;
 		for(int i = 0;i<edge;i++){
