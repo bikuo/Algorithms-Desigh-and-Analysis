aac3d679a6717a76360ffee127f7a94af1fee016
williamkuo <lock3262000@gmail.com>
2015-01-05 03:21:31 +0800

    uccu

--- 10/10.cpp
+++ 10/10.cpp
@@ -19,27 +19,13 @@ int main(int argc, char const *argv[]){
 		}
 		if(intel[1] < intel[p])
 			ask[1] = p;
-		/*if(ask[1] == -1){
-			if(intel[1] == max)
-				ask[1] = 0;
-			else{
-				int check = p;
-				while(1){
-					if(intel[check] > intel[1]){
-						ask[1] = check;
-						break;
-					}
-					--check;
-				}
-			}
-		}*/
 		int i=maxer;
 		int rounds = p;
 		while (rounds--){
 			if(ask[i] == -1){
 				int temp = (i==1)? p:i-1;
 				if(intel[i] == max)
-					ask[i] = i;
+					ask[i] = 0;
 				else if(intel[i] == intel[temp])
 					ask[i] = ask[temp];
 				else if(intel[i] > intel[temp]){
@@ -47,22 +33,17 @@ int main(int argc, char const *argv[]){
 					while(1){
 						if(intel[check] > intel[i]){
 							ask[i] = check;
-							//printf("whatthe i = %d\n", i);
 							break;
 						}
 						check = ask[check];
 					}
 				} 
 			}
-			i = i+1; 
-			if (i > p) i = 1;
-			//printf("!! i == %d\n", i);
+			i = (i == p)? 1:i+1;
 		}
 		for (i = 1; i <= p; ++i){
-			if(ask[i] == i)ask[i] = 0;
 			(i==p)? printf("%d\n",ask[i]):printf("%d ",ask[i]);
 		}
-	
 	}
 	return 0;
 }
